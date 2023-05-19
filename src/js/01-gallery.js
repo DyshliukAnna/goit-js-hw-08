59d0c2fe09efa261697f854c8fb213102ba59a62
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-19 14:01:01 +0300

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-
+  
 console.log(galleryItems);
 const galleryBox = document.querySelector(".gallery");
 

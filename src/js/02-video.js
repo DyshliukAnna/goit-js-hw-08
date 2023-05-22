d2dc1b13813cdc6b08ea91b1082ecda0f3a6704c
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-22 12:51:43 +0300

    2 ex change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,35 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const KEY = "videoplayer-current-time"
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+
+player.setCurrentTime(currentTime);
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on(' timeupdate ', throttle(update (), 1000));
+
+// player.setCurrentTime(currentTime);
+
+
+// function update({ seconds }) {
+//     localStorage.setItem(KEY, `${seconds}`)
+    
+// }
+// const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
+
+// player.setCurrentTime(currentTime);

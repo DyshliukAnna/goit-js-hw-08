d32b3b84e55bfa26d14a0bf0284b744e52f0425d
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-22 15:50:35 +0300

    ex 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,30 @@
-import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
 
 const KEY = "videoplayer-current-time"
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(({ seconds }) => {
-    localStorage.setItem('videoplayer-current-time', `${seconds}`);
-  }, 1000)
-);
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', throttle(update, 1000));
+    
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+function update({seconds}) {
+    localStorage.setItem(KEY, seconds);
+}
 
 const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
 
 player.setCurrentTime(currentTime);
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// player.on(' timeupdate ', throttle(update (), 1000));
-
-// player.setCurrentTime(currentTime);
 
 
-// function update({ seconds }) {
-//     localStorage.setItem(KEY, `${seconds}`)
-    
-// }
-// const currentTime = localStorage.getItem('videoplayer-current-time') || 0;
 
-// player.setCurrentTime(currentTime);
+
+
+
